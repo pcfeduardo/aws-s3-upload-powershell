8c022811129ef5cd58f5f7176465591e08f54292
Carlos Eduardo Vieira <pcfeduardo@gmail.com>
2023-04-30 09:57:38 -0300

    feat: directory or file upload

--- main.ps1
+++ main.ps1
@@ -5,23 +5,38 @@ $accessKey = $jsonContent.accessKey
 $secretKey = $jsonContent.secretKey
 $region = $jsonContent.region
 $bucketName = $jsonContent.bucketName
-$filePathList = $jsonContent.filePathList
+$pathsList = $jsonContent.pathsList
 
 Import-Module AWS.Tools.Installer
 Set-AWSCredential -AccessKey $accessKey -SecretKey $secretKey -StoreAs default
 Initialize-AWSDefaults -ProfileName default -Region $region
 
-foreach ($filePath in $filePathList) {
-    if (-not (Test-Path -Path $filePath)) {
-        Write-Host "File not found: $filePath" -ForegroundColor Red
+foreach ($path in $pathsList) {
+    if (-not (Test-Path -Path $path)) {
+        Write-Host "Path not found: $path" -ForegroundColor Red
         continue
     }
 
-    try {
-        Write-Host "Uploading file $filePath to bucket $bucketName" -ForegroundColor Yellow
-        Write-S3Object -BucketName $bucketName -File $filePath -Key (Split-Path $filePath -Leaf) -CannedACLName private -ProfileName default
-    }
-    catch {
-        Write-Host "Error when performing multipart upload to file: $filePath. Erro: $($_.Exception.Message)" -ForegroundColor Red
+    if (Test-Path -Path $path -PathType Container) {
+        try {
+            Write-Host "Uploading folder $path to bucket $bucketName" -ForegroundColor Yellow
+            $folderName = Split-Path $path -Leaf
+            Get-ChildItem $path -Recurse | ForEach-Object {
+                $relativePath = $_.FullName.Substring($path.Length).TrimStart('\')
+                $key = (Join-Path $folderName $relativePath).Replace('\', '/')
+                Write-S3Object -BucketName $bucketName -File $_.FullName -Key $key -CannedACLName private -ProfileName default
+            }
+        }
+        catch {
+            Write-Host Error when uploading directory: $path. Erro: $($_.Exception.Message)" -ForegroundColor Red
+        }
+    } elseif (Test-Path -Path $path -PathType Leaf) {
+        try {
+            Write-Host "Uploading file $path to bucket $bucketName" -ForegroundColor Yellow
+            Write-S3Object -BucketName $bucketName -File $path -Key (Split-Path $path -Leaf) -CannedACLName private -ProfileName default
+        }
+        catch {
+            Write-Host "Error when uploading file: $path. Erro: $($_.Exception.Message)" -ForegroundColor Red
+        }
     }
 }
